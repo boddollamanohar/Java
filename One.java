7e8458ba5929d93b59ee8f2ac7bbff06d2fda318
unknown <iambmanohar@gmail.com>
2023-06-12 12:50:45 +0530

    data

---
+++ One.java
@@ -0,0 +1,7 @@
+Class One{
+	public static void main(String[]args){
+
+System.out.println("hello good morning");
+
+}
+}
---
+++ Two.java
@@ -0,0 +1,8 @@
+Class dog {
+Public string breed = " German shepard";
+Public int height = 30;
+Public int weight = 35;
+Public static void main ( string[] args);
+System.out,println("test");
+Dog dog = new Dog();
+System.out.println(dog.breed);
